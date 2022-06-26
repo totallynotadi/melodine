0de5eef038a4ab61b9f3380d32f9029458443ba2
addyett <g.aditya2048@gmail.com>
2022-06-27 00:16:45 +0530

    fix: model player track to Track object

--- melo/models/spotify/player.py
+++ melo/models/spotify/player.py
@@ -9,11 +9,13 @@ from .track import Track
 @dataclass
 class PlayerContext:
     '''the "Playing From" context for particular playback state'''
+
     def __init__(self, data: Dict) -> None:
         self.external_urls: List[Dict[str, str]] = data.get('external_urls')
         self.type: str = data.get('type')
         self.uri: str = data.get('uri')
         self.href: str = f'open.spotify.com/{type}/{self.uri.split(":")[2]}'
+
 
 class Player:
     def __init__(self, data: Dict, user: "spotify.User") -> None:
@@ -24,11 +26,11 @@ class Player:
         self.repeat_state: bool = data.get('repeat_state')
         self.context: PlayerContext = data.get('context')
         self.progress = data.get('progress_ms') * 1000
-        self.item: Track = data.get('item')
+        self.item: Track = Track(data.get('item'))
         self.currently_playing_type = data.get('currently_playing_type')
         self.is_playing: bool = data.get('is_playing')
 
     def __repr__(self):
         return f"<spotify.Player: {self.user!r}>"
 
-    #TODO - check if basic player modifications works for non-premium users and implement those methods
+    # TODO - check if basic player modifications works for non-premium users and implement those methods
