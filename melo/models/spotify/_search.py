11ab0fe89ab7de932d0fea0f56a00acb9cfabb9c
addyett <g.aditya2048@gmail.com>
2022-10-02 16:18:29 +0530

    fix: manage search modules

--- melo/models/spotify/_search.py
+++ melo/models/spotify/_search.py
@@ -1,28 +1,28 @@
 from dataclasses import dataclass, field
 from typing import Iterable, List, Optional
 
-from ...utils import SPOTIFY, SearchResults
-from . import (
-    Artist,
+from melo.utils import SearchResults
+from melo.configs import SPOTIFY
+from melo.models.spotify import (
     Album,
-    Track,
-    Playlist,
+    Artist,
     Episode,
-    Show
+    Playlist,
+    Show,
+    Track
 )
-
 
 __all__ = ["search"]
 
 _TYPES = {"artist": Artist, "album": Album, "track": Track,
           "playlist": Playlist, "show": Show, "episode": Episode}
-_SEARCH_TYPES = {"artist", "album", "track", "playlist", "show", "episode"}
+_SEARCH_TYPES = {"artists", "albums", "tracks", "playlists", "shows", "episodes"}
 
 @dataclass
 class SpotifySearchResults(SearchResults):
     ''' A dataclass for Search Results
 
-    Inherits from the base SearchResults class to add some attributes specifc to Spotify
+    Inherits from the base SearchResults class.
 
     Attributes
     ----------
@@ -37,22 +37,27 @@ class SpotifySearchResults(SearchResults):
     videos: List[:class:`Video`]
         The videos from the search results
     '''
+    
+    artists: Optional[List[Artist]] = field(default_factory=list)
+    albums: Optional[List[Album]] = field(default_factory=list)
+    tracks: Optional[List[Track]] = field(default_factory=list)
+    playlists: Optional[List[Playlist]] = field(default_factory=list)
 
-    shows: Optional[List] = field(default_factory=list)
-    episodes: Optional[List] = field(default_factory=list)
+    shows: Optional[List[Show]] = field(default_factory=list)
+    episodes: Optional[List[Episode]] = field(default_factory=list)
 
 
 def search(
     q: str,  # pylint: disable=invalid-name
     *,
-    types: Iterable[str] = ("track", "playlist", "artist", "album", "show", "episode"),
+    types: Iterable[str] = ("tracks", "playlists", "artists", "albums", "shows", "episodes"),
     limit: int = 20,
     offset: int = 0,
 ) -> SpotifySearchResults:
     '''Get search results for a query'''
 
     if types is None:
-        types = ("track", "playlist", "artist", "album", "show", "episode")
+        types = ("track", "playlists", "artists", "albums", "shows", "episodes")
 
     if not hasattr(types, "__iter__"):
         raise TypeError("types must be an iterable.")
@@ -60,9 +65,11 @@ def search(
     types_ = set(types)
 
     if not types_.issubset(_SEARCH_TYPES):
-        raise ValueError('Bad queary type! got "%s" expected any of: track, playlist, artist, album' %
+        raise ValueError('Bad queary type! got "%s" expected any of: tracks, playlists, artists, albums, shows, episodes' %
                          types_.difference(_SEARCH_TYPES).pop())
 
+    types = list(map(lambda type: type[:-1] if type.endswith('s') else type, types))
+    
     query_type = ",".join(tp.strip() for tp in types)
 
     data = SPOTIFY.search(
