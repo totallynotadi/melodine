381c404b5e85f6d8da344b582e7c630b4a4845cf
addyett <g.aditya2048@gmail.com>
2022-12-06 18:14:25 +0530

    fix: threaded fade-in

--- melodine/player/helpers.py
+++ melodine/player/helpers.py
@@ -16,49 +16,56 @@ FF_OPTS = {
     'infbuf': True
 }
 
+
 def close_player(player):
     print(f':: CLOSING PLAYER at {player.get_pts()}')
     player.toggle_pause()
     time.sleep(1)
     player.close_player()
 
+
 def close_stream(player: MediaPlayer, fade: bool = True) -> None:
     # print(f'::: current_pts: {current_pts}, {DURATION - current_pts} seconds left')
 
     DURATION = player.get_metadata()['duration']
-    
+
     print('::: entered closing')
-    if fade:     
+    if fade:
         player_fade_out(player)
-    #     for step in FADE_OUT_STEPS[FADE_OUT_STEPS.index(player.get_volume()):]:
-    #         delta = DURATION - player.get_pts()
-    #         player.set_volume(step)
-    #         print(player.get_volume(), player.get_pts(), delta, step)
-    #         time.sleep(delta / SMOOTHNESS_FACTOR)
-    # else:   close_player(player)
     print(':: BROKE')
-    
+
+
 def player_fade_in(player: MediaPlayer, fade: int = 0):
     for step in FADE_IN_STEPS:
-            delta = fade - player.get_pts()
-            player.set_volume(step)
-            print(player.get_volume(), player.get_pts(), delta, step)
-            time.sleep(delta / SMOOTHNESS_FACTOR)
-            
+        delta = fade - player.get_pts()
+        player.set_volume(step)
+        print(player.get_volume(), player.get_pts(), delta, step)
+        time.sleep(delta / SMOOTHNESS_FACTOR)
+
+
 def player_fade_out(player: MediaPlayer):
     DURATION = player.get_metadata()['duration']
-    
+
     print('::: entered closing')
     for step in FADE_OUT_STEPS[FADE_OUT_STEPS.index(player.get_volume()):]:
-            delta = DURATION - player.get_pts()
-            player.set_volume(step)
-            print(player.get_volume(), player.get_pts(), delta, step)
-            time.sleep(delta / SMOOTHNESS_FACTOR)
-    else:   close_player(player)
-    
-def manage_stream(player: MediaPlayer, source: str, closing_ref: int = 0, fade_out: bool = True) -> None:
+        delta = DURATION - player.get_pts()
+        player.set_volume(step)
+        print(player.get_volume(), player.get_pts(), delta, step)
+        time.sleep(delta / SMOOTHNESS_FACTOR)
+    else:
+        close_player(player)
+
+
+def manage_stream(player: MediaPlayer, source: str, fade: int = 0, fade_in: bool = True) -> None:
     last_pts = 10
     updated_pts = 0
+
+    threading.Thread(
+        target=player_fade_in,
+        args=(player, fade)
+    ).start() if fade_in else player.set_volume(1.0)
+    if player.get_pause():
+        player.toggle_pause()
 
     DURATION = player.get_metadata()['duration']
 
@@ -78,9 +85,8 @@ def manage_stream(player: MediaPlayer, source: str, closing_ref: int = 0, fade_o
             player.toggle_pause()
             print(f"buffered out, pausing: {buffer_repeat_count}")
             # player.seek(-1, relative=True, accurate=False)
-            time.sleep(1)
             player.toggle_pause()
-            time.sleep(1)
+            time.sleep(2)
             updated_pts = int(str(player.get_pts()).split('.', maxsplit=1)[0])
             last_buffered_pts = last_pts
             if last_buffered_pts == updated_pts:
@@ -107,18 +113,21 @@ def manage_stream(player: MediaPlayer, source: str, closing_ref: int = 0, fade_o
             player.toggle_pause()
             buffer_repeat_count = 0
 
+        # fade aka refenrece point to begin closing
         current_pts = int(str(player.get_pts()).split('.', maxsplit=1)[0])
-        if current_pts + abs(closing_ref) >= DURATION:
-            if closing_ref <= 3:
-                if closing_ref > 0:
-                    time.sleep(closing_ref)
+        if current_pts + abs(fade) >= DURATION:
+            if fade <= 3:
+                if fade > 0:
+                    time.sleep(fade)
                 buffer_repeat_count = 0
                 close_player(player)
                 break
             print(
                 f"breaking from the handler thread, at timestamp: {current_pts}")
-            threading.Thread(target=close_stream,
-                             args=(player, fade_out)).start()
+            threading.Thread(
+                target=close_stream,
+                args=(player, bool(fade))
+            ).start()
             break
         last_pts = updated_pts
         time.sleep(1)
