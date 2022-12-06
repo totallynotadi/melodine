3af9e744eb9fbcb0b51a26c2bc8612f08f3c44b6
addyett <g.aditya2048@gmail.com>
2022-12-06 18:17:45 +0530

    fix: remove static fade-in

--- melodine/player/players.py
+++ melodine/player/players.py
@@ -11,8 +11,8 @@ from melodine.utils import singleton
 
 FF_OPTS = {
     'paused': True,
-    'vn': True,
     'sync': 'audio',
+    'vn': True,
     'genpts': True,
     'infbuf': True
 }
@@ -21,8 +21,9 @@ FF_OPTS = {
 def playsound(location: Union[str, BinaryIO], *, blocking: bool = False):
     file_name = None
     if isinstance(location, io.IOBase):
+        file_mode = 'w' + 'b' if isinstance(location, io.FileIO) else ''
         file_name = os.path.join(TEMPFILES_DIR, id(location))
-        with open(file_name) as file:
+        with open(file_name, file_mode) as file:
             file.write(location.read())
         location = file_name
 
@@ -42,22 +43,12 @@ def playsound(location: Union[str, BinaryIO], *, blocking: bool = False):
 
 
 def play(location: str, *, blocking: bool = False, fade: int = 0, fade_in: bool = False) -> None:
-    filters = 'silenceremove=stop_periods=1:stop_threshold=0dB:stop_mode=any:detection=peak'
-    # filters = 'silenceremove=start_periods=1:start_duration=1:start_threshold=-50dB:detection=peak,areverse,silenceremove=start_periods=1:start_duration=1:start_threshold=-60dB:detection=peak,areverse'
-    # filters = ''
-    FF_OPTS.update({'volume': 0.0, 'af': filters})
-    player = MediaPlayer(location, ff_opts=FF_OPTS)
-    # time.sleep(1)
-
-    time.sleep(2)
+    player = MediaPlayer(location, loglevel='quiet', ff_opts={
+                         **FF_OPTS, **{'volume': 0.0}}, thread_lib="SDL")
+    time.sleep(1)
     player.set_volume(0.0)
-    player.toggle_pause()
-    # time.sleep(1)
+    time.sleep(1)
 
-    if fade_in:
-        player_fade_in(player, fade)
-    else:   player.set_volume(1.0)
-    
     manager_thread = threading.Thread(
         target=manage_stream, args=(player, location, fade))
     if blocking:
@@ -71,12 +62,11 @@ class Player:
     def __new__(self, autoplay: bool = False, crossfade: int = 6):
         self.crossfade = crossfade
         self.autoplay = autoplay
-        
         self.now_playing = None
         self.queue = []
-        
+
         threading.Thread(target=self.player_handler, args=(self,)).start()
-    
+
     def player_handler(self):
         while True:
             for track in self.queue:
