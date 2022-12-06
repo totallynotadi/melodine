6d2923e2b52f81258f3766166c75486b3e522d31
addyett <g.aditya2048@gmail.com>
2022-12-06 19:55:31 +0530

    feat: finish base player

--- melodine/player/players.py
+++ melodine/player/players.py
@@ -2,12 +2,14 @@ import io
 import os
 import threading
 import time
-from typing import BinaryIO, Union
+from typing import BinaryIO, List, Union
 
 from ffpyplayer.player import MediaPlayer
+
 from melodine.configs import TEMPFILES_DIR
 from melodine.player.helpers import manage_stream, player_fade_in
 from melodine.utils import singleton
+import melodine
 
 FF_OPTS = {
     'paused': True,
@@ -42,7 +44,7 @@ def playsound(location: Union[str, BinaryIO], *, blocking: bool = False):
         os.remove(file_name)
 
 
-def play(location: str, *, blocking: bool = False, fade: int = 0, fade_in: bool = False) -> None:
+def play(location: str, *, blocking: bool = False, fade: int = 0, fade_in: bool = False) -> Union[MediaPlayer, None]:
     player = MediaPlayer(location, loglevel='quiet', ff_opts={
                          **FF_OPTS, **{'volume': 0.0}}, thread_lib="SDL")
     time.sleep(1)
@@ -53,23 +55,99 @@ def play(location: str, *, blocking: bool = False, fade: int = 0, fade_in: bool 
         target=manage_stream, args=(player, location, fade))
     if blocking:
         manager_thread.run()
+    elif blocking is None:
+        return player
     else:
         manager_thread.start()
+        return player
 
 
 @singleton
 class Player:
-    def __new__(self, autoplay: bool = False, crossfade: int = 6):
-        self.crossfade = crossfade
-        self.autoplay = autoplay
-        self.now_playing = None
-        self.queue = []
+    def __init__(self, autoplay: bool = False, crossfade: int = 6):
+        self.crossfade: int = crossfade
+        self.autoplay: bool = autoplay
 
-        threading.Thread(target=self.player_handler, args=(self,)).start()
+        self.shuffle: bool = False
+        self.repeast: bool = False
+
+        self.recently_played: List = []
+        self.now_playing = None
+        self.__now_playing = None
+        self.queue: List = []
+
+        threading.Thread(target=self.player_handler, args=()).start()
 
     def player_handler(self):
+        sleep_time = 0
         while True:
             for track in self.queue:
-                self.now_playing = track
                 self.queue.remove(track)
+                self.now_playing = track
+                self.__now_playing = play(
+                    self.now_playing.url,
+                    blocking=True,
+                    fade=self.crossfade
+                )
+                self.recently_played.append(track)
+
             time.sleep(0.4)
+
+    # pause state
+    def get_state(self) -> bool:
+        return self.__now_playing.get_pause()
+
+    def set_state(self, state: bool) -> None:
+        self.__now_playing.set_pause(state)
+
+    def toggle_state(self) -> None:
+        self.__now_playing.toggle_pause()
+
+    # player volume
+    def get_volume(self) -> int:
+        return self.__now_playing.get_volume()
+
+    def set_volume(self, volume: int) -> None:
+        self.__now_playing.set_volume(volume)
+
+    # mute
+    def get_mute(self) -> bool:
+        return self.__now_playing.get_mute()
+
+    def set_mute(self, mute: bool) -> None:
+        self.__now_playing.set_pause(mute)
+
+    def toggle_mute(self) -> None:
+        self.__now_playing.set_mute(not self.__now_playing.get_mute())
+
+    # playback progress
+    def get_current_timestamp(self) -> int:
+        return int(self.__now_playing.get_pts())
+
+    def seek(self, pts: int) -> None:
+        self.__now_playing.seek(pts, relative=False, accurate=False)
+
+    def get_shuffle(self) -> bool:
+        return self.shuffle
+
+    def set_shuffle(self, shuffle: bool) -> None:
+        self.shuffle = shuffle
+
+    def toggle_shuffle(self) -> None:
+        self.shuffle = not self.shuffle
+
+    # queing
+    def play_next(self, track):
+        self.queue.insert(0, track)
+
+    def add_to_queue(self, track):
+        self.queue.append(track)
+
+    def remove_from_queue(self, track: Union[int, "melodine.track.Track"]) -> None:
+        if isinstance(track, int):
+            del self.queue[track]
+        else:
+            self.queue.remove(track)
+
+    def play(self, track) -> None:
+        self.add_to_queue(track)
